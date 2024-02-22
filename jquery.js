4a34ec809d22e5d5ef95333af023b6fc49eb1450
TaysWeb <snelltararobin@gmail.com>
2024-02-22 17:00:16 +0200

    fixed some things

--- jquery.js
+++ jquery.js
@@ -1,30 +1,34 @@
-$('document').ready(function(){
-    // next button click event
-    $('.next').click(function(){
-      // select image slides
-      let prev = document.querySelector('.previous'); 
-      let next = document.querySelector('.next'); 
-      // remove class 
-      $(prev).removeClass('previous'); 
-      $(next).removeClass('next'); 
-      
-      // add class 
-      $(prev).addClass('next'); 
-      $(next).addClass('previous'); 
-    })
-    
-    // prev button click event
-    $('.previous').click(function(){
-      // select image slides
-      let prev = document.querySelector('.prev'); 
-      let next = document.querySelector('.next'); 
-      
-      // remove class 
-      $(prev).removeClass('previous'); 
-      $(next).removeClass('next'); 
-      
-      // add class 
-      $(prev).addClass('next'); 
-      $(next).addClass('previous'); 
-    })
+$(document).ready(function () {
+  let currentSlide = 0;
+  const slides = $('.slide');
+  let circles = $('.circle');
+
+  function showSlide(index) {
+      currentSlide = (index + slides.length) % slides.length;
+      slides.css('transform', `translateX(${-99 * currentSlide}%)`);
+      circles.removeClass('active');
+      circles.eq(currentSlide).addClass('active');
+  }
+
+  function changeSlide(offset) {
+      showSlide(currentSlide + offset);
+  }
+
+  $('.arrow ').on('click', function () {
+      changeSlide(1);
   });
+
+  $('.circles ').on('click', '.circle', function () {
+      const cirIndex = $(this).index();
+      showSlide(cirIndex);
+  });
+
+  for (let i = 0; i < slides.length; i++) {
+      $('.circles ').append('<div class="circle"></div>');
+  }
+
+circles = $('.circles'); 
+
+  showSlide(currentSlide);
+
+});
