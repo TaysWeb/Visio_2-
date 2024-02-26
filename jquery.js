53c2b0c2927db7eb88e760e24d22695cbd6f4e3d
Taysweb <snelltararobin@gmail.com>
2024-02-26 12:55:31 +0200

    fixing jquery

--- jquery.js
+++ jquery.js
@@ -1,11 +1,46 @@
+// $(document).ready(function () {
+//   let currentSlide = 0;
+//   const slides = $('.slide');
+//   let circles = $('.circle');
+
+//   function showSlide(index) {
+//       currentSlide = (index + slides.length) % slides.length;
+//       slides.css('transform', `translateX(${-99 * currentSlide}%)`);
+//       circles.removeClass('active');
+//       circles.eq(currentSlide).addClass('active');
+//   }
+
+//   function changeSlide(offset) {
+//       showSlide(currentSlide + offset);
+//   }
+
+//   $('.arrow').on('click', function () {
+//       changeSlide(1);
+//   });
+
+//   $('.circles ').on('click', '.circle', function () {
+//       const cirIndex = $(this).index();
+//       showSlide(cirIndex);
+//   });
+
+//   for (let i = 0; i < slides.length; i++) {
+//       $('.circles ').append('<div class="circle"></div>');
+//   }
+
+// circles = $('.circles'); 
+
+//   showSlide(currentSlide);
+
+// });
+
 $(document).ready(function () {
   let currentSlide = 0;
   const slides = $('.slide');
-  let circles = $('.circle');
+  let circlesContainer = $('.circles'); // Corrected variable name
 
   function showSlide(index) {
       currentSlide = (index + slides.length) % slides.length;
-      slides.css('transform', `translateX(${-99 * currentSlide}%)`);
+      slides.css('transform', `translateX(${-100 * currentSlide}%)`); // Corrected transform value
       circles.removeClass('active');
       circles.eq(currentSlide).addClass('active');
   }
@@ -18,17 +53,16 @@ $(document).ready(function () {
       changeSlide(1);
   });
 
-  $('.circles ').on('click', '.circle', function () {
+  circlesContainer.on('click', '.circle', function () { // Corrected selector
       const cirIndex = $(this).index();
       showSlide(cirIndex);
   });
 
   for (let i = 0; i < slides.length; i++) {
-      $('.circles ').append('<div class="circle"></div>');
+      circlesContainer.append('<div class="circle"></div>');
   }
 
-circles = $('.circles'); 
+  circles = circlesContainer.find('.circle'); // Removed unnecessary reassignment
 
   showSlide(currentSlide);
-
 });
